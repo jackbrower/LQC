5da7445ccefa98778befd4401db97d200a468160
Jack Brower <jackbrower@duck.com>
2022-02-28 20:59:42 -0500

    Finalize script

--- install.sh
+++ install.sh
@@ -3,19 +3,59 @@
 echo "Installing LQC."
 
 # Replace MINT with DEBIAN
-distro=$(grep -oE 'Mint|Archlinux' /etc/issue)
+distro=$(grep -oE 'Debian|Archlinux' /etc/issue)
 install="$HOME/.local/src/jackbrower"
 
-if [ 'Mint' = "$distro" ]; then
+if [ 'Debian' = "$distro" ]; then
   echo "Debian detected, continuing script..."
-  echo "Instaling git from distro package manager: "
+  echo "Instaling git via apt: "
   sudo apt install git
+  echo "Installing x11 via apt: "
+  sudo apt install xorg
   echo "Fetching dwm, st, dmenu, slstatus..."
-  rm -rf $distro
+  rm -rf $install
   mkdir -p $install
   git clone https://github.com/jackbrower/dmenu.git $install/dmenu
+  git clone https://github.com/jackbrower/slstatus.git $install/slstatus
+  git clone https://github.com/jackbrower/dwm.git $install/dwm
+  git clone https://github.com/jackbrower/st.git $install/st
+  echo "Compiling..."
+  cd $install/dmenu
+  sudo make clean install
+  cd $install/slstatus
+  sudo make clean install
+  cd $install/dwm
+  sudo make clean install
+  cd $install/st
+  sudo make clean install
+  echo "Configuring .xinitrc..."
+  rm -f $HOME/.xinitrc
+  echo -e "dmenu &\n slstatus &\n exec dwm" > $HOME/.xinitrc
+  exec startx
 elif [ 'Archlinux' = "$distro" ]; then
   echo "Arch detected, continuing script..."
+  echo "Installing x11 via apt: "
+  sudo pacman -Ss xorg xf86-video
+  echo "Fetching dwm, st, dmenu, slstatus..."
+  rm -rf $install
+  mkdir -p $install
+  git clone https://github.com/jackbrower/dmenu.git $install/dmenu
+  git clone https://github.com/jackbrower/slstatus.git $install/slstatus
+  git clone https://github.com/jackbrower/dwm.git $install/dwm
+  git clone https://github.com/jackbrower/st.git $install/st
+  echo "Compiling..."
+  cd $install/dmenu
+  sudo make clean install
+  cd $install/slstatus
+  sudo make clean install
+  cd $install/dwm
+  sudo make clean install
+  cd $install/st
+  sudo make clean install
+  echo "Configuring .xinitrc..."
+  rm -f $HOME/.xinitrc
+  echo -e "dmenu &\n slstatus &\n exec dwm" > $HOME/.xinitrc
+  exec startx
 else
   echo "Invalid distribution, exiting."
   exit 1
